73e6f140dcdd45add8c5a0327b222646cf965254
Yasitha Charman <yasitha.charman@gmail.com>
2022-12-03 08:53:34 +0530

    Added initial pom files and sources

---
+++ src/main/java/dev/yasitha/deliverydrone/MydeliverydronApplication.java
@@ -0,0 +1,13 @@
+package dev.yasitha.deliverydrone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydeliverydronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydeliverydronApplication.class, args);
+	}
+
+}
